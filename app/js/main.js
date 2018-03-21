be6e4e911cba08f7b74759a3dff84d632628b0de
formation <heihong.traing@soat.fr>
2018-03-21 18:46:22 +0100

    recupe_json

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+var request = new XMLHttpRequest();
+   request.open("GET", "js/fdv2017.json", false);
+   request.send(null)
+   var my_JSON_object = JSON.parse(request.responseText);
+
+let Vins = my_JSON_object;
+
+
+
+console.log(Vins);
